acd95600db74e002c97c1e1a6106034627bd85c0
Jan Ehrhardt <jan.ehrhardt@gmail.com>
2014-07-07 10:45:30 +0200

    Add Disqus comments again

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,7 +15,8 @@ module.exports = function(grunt) {
       app: {
         files: {
           'assets/js/app.js': ['_js/twitter.js',
-                               '_js/google-analytics.js'],
+                               '_js/google-analytics.js',
+                               '_js/disqus.js'],
           'assets/js/html5shiv.js': 'bower_components/html5shiv/dist/html5shiv-printshiv.js',
           'assets/js/respond.js': 'bower_components/respond/src/respond.js'
         }
---
+++ _js/disqus.js
@@ -0,0 +1,9 @@
+(function() {
+  var disqusScript = document.createElement('script');
+
+  disqusScript.type = 'text/javascript';
+  disqusScript.async = true;
+  disqusScript.src = '//derjanio.disqus.com/embed.js';
+
+  document.getElementsByTagName('body')[0].appendChild(disqusScript);
+})();
